9a54cf9a49c91da2d1d925b8197d8f30c4f80af1
Villager <deiondrae@gmail.com>
2022-03-07 19:14:33 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,8 @@
+var randomNumber = function(min, max){
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -5,7 +10,6 @@ var playerMoney = 10;
 console.log(playerHealth)
 
 var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
@@ -22,13 +26,15 @@ var fight = function (enemyName) {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             //subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }
     }
         //remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage)
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining "
         );
@@ -44,7 +50,9 @@ var fight = function (enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
         //remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName+ " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining "
         );
@@ -62,6 +70,7 @@ var startGame = function() {
     playerHealth = 100;
     playerAttack = 10;
     playerMoney = 10;
+    
     for (var i = 0; i < enemyNames.length; i++) {
         if (playerHealth > 0) {
             // Let player know round number
@@ -71,7 +80,7 @@ var startGame = function() {
         var pickedEnemyName = enemyNames[i];
 
         // reset enemyHealth before new fight
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
 
         //use debugger to pause script 
         //debugger;
